a7607f41283ebd4909e2fd1515406078c7869d40
Rola Diaz <rolandodiazjara@gmail.com>
2024-01-19 16:15:27 -0500

    antes purga

--- Enjs/firmar.js
+++ Enjs/firmar.js
@@ -3,13 +3,19 @@ var fs = require("fs");
 var xpath = require("xpath");
 var dom = require("xmldom").DOMParser;
 
+// Lee la clave pública desde un archivo y elimina las líneas "-----BEGIN CERTIFICATE-----" y "-----END CERTIFICATE-----"
+var publicKey = fs.readFileSync("certificate.pem", "utf8");
+publicKey = publicKey.replace(/-----BEGIN CERTIFICATE-----/g, "");
+publicKey = publicKey.replace(/-----END CERTIFICATE-----/g, "");
+publicKey = publicKey.trim();
+
 // Lee el archivo XML
 var xml = fs.readFileSync("xml_sinfirma.xml", "utf8");
 var doc = new dom().parseFromString(xml);
 
 var sig = new SignedXml({
   privateKey: fs.readFileSync("private_key.pem"),
-  publicCert: fs.readFileSync("certificate.pem"),
+  publicCert: publicKey,
 });
 sig.addReference({
   xpath: "//*[local-name(.)='ExtensionContent']",
@@ -19,7 +25,6 @@ sig.addReference({
 sig.canonicalizationAlgorithm = "http://www.w3.org/2001/10/xml-exc-c14n#";
 sig.signatureAlgorithm = "http://www.w3.org/2000/09/xmldsig#rsa-sha1";
 sig.computeSignature(xml);
-fs.writeFileSync("xml_firmado.xml", sig.getSignedXml());
 
 // Encuentra el elemento ExtensionContent y añade la firma
 var select = xpath.select;
@@ -29,5 +34,10 @@ var parser = new dom();
 var signatureNode = parser.parseFromString(signature);
 nodes[0].appendChild(signatureNode);
 
-// Guarda el XML firmado
+// Añade la clave pública al XML firmado
+var publicKeyNode = doc.createElement("PublicKey");
+publicKeyNode.textContent = publicKey;
+nodes[0].appendChild(publicKeyNode);
+
+// Guarda el XML firmado con la clave pública
 fs.writeFileSync("xml_firmado.xml", doc.toString());
